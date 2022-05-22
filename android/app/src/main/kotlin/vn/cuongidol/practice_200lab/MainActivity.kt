f4410f7a9894e0a77967c11ab28b73c9a7ec1642
quachcuong1990 <quachcuongvfu@gmail.com>
2022-05-22 13:33:48 +0700

    init project

---
+++ android/app/src/main/kotlin/vn/cuongidol/practice_200lab/MainActivity.kt
@@ -0,0 +1,6 @@
+package vn.cuongidol.practice_200lab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  child: Column(
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: Container(
+                            child: Row(
+                              children: [
+                                Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      child: Column(
+                                        children: [
+                                          Expanded(
+                                              flex: 1,
+                                              child: Container(
+                                                color: Colors.red,
+                                              )),
+                                          Expanded(
+                                              flex: 1,
+                                              child: Container(
+                                                color: Colors.yellow,
+                                              )),
+                                          Expanded(
+                                              flex: 1,
+                                              child: Container(
+                                                color: Colors.blue,
+                                              )),
+                                          Expanded(
+                                              flex: 1,
+                                              child: Container(
+                                                color: Colors.white,
+                                              )),
+                                        ],
+                                      ),
+                                    )),
+                                Expanded(
+                                    flex: 2,
+                                    child: Container(
+                                      child: Column(
+                                        children: [
+                                          Expanded(
+                                              flex: 3,
+                                              child: Container(
+                                                color: Colors.cyan,
+                                              )),
+                                          Expanded(
+                                              flex: 1,
+                                              child: Container(
+                                                child: Row(
+                                                  children: [
+                                                    Expanded(
+                                                        flex: 1,
+                                                        child: Container(
+                                                          color:
+                                                              Colors.lightBlue,
+                                                        )),
+                                                    Expanded(
+                                                        flex: 1,
+                                                        child: Container(
+                                                          color: Colors.green,
+                                                        )),
+                                                  ],
+                                                ),
+                                              )),
+                                        ],
+                                      ),
+                                    )),
+                              ],
+                            ),
+                          )),
+                      Expanded(
+                          flex: 2,
+                          child: Container(
+                            color: Colors.black,
+                          )),
+                      Expanded(
+                          flex: 2,
+                          child: Container(
+                            color: Colors.yellow,
+                          )),
+                      Expanded(flex: 2, child: Container()),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.red.shade200,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  margin: EdgeInsets.only(left: 10),
+                  color: Colors.red.shade200,
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 350,
+            left: 50,
+            child: Container(
+                width: 300, height: 150, color: Colors.black.withOpacity(0.2)),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
